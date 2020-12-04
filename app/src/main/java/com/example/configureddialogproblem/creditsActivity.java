65d3878b9d8597de807666d048476bce3ceaf0cb
AndriyKuzmenko <59597557+AndriyKuzmenko@users.noreply.github.com>
2020-12-04 19:06:52 +0200

    Update creditsActivity.java

--- app/src/main/java/com/example/configureddialogproblem/creditsActivity.java
+++ app/src/main/java/com/example/configureddialogproblem/creditsActivity.java
@@ -3,12 +3,20 @@ package com.example.configureddialogproblem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class creditsActivity extends AppCompatActivity {
+public class creditsActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+    }
+
+    public void back(View view)
+    {
+        finish();
     }
 }
