f3807ab4172490cad05f9e07867b323968a65b17
AndriyKuzmenko <59597557+AndriyKuzmenko@users.noreply.github.com>
2020-12-04 18:17:08 +0200

    First Alert Dialog

--- app/src/main/java/com/example/configureddialogproblem/MainActivity.java
+++ app/src/main/java/com/example/configureddialogproblem/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.configureddialogproblem;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    AlertDialog.Builder adb;
+    final String[] colors={"Red","Green","Blue"};
+    int[] color;
+    LinearLayout layout;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layout=(LinearLayout)findViewById(R.id.layout);
+    }
+
+    public void button1Pressed(View view)
+    {
+        color=new int[]{0,0,0};
+        adb=new AlertDialog.Builder(this);
+
+        adb.setTitle("Alrt Dialog 1");
+        adb.setItems(colors, new DialogInterface.OnClickListener()
+        {
+           @Override
+           public void onClick(DialogInterface dialog, int which)
+           {
+               color[which]=255;
+               layout.setBackgroundColor(Color.rgb(color[0],color[1],color[2]));
+           }
+        });
+
+        AlertDialog ad=adb.create();
+        ad.show();
     }
 }
